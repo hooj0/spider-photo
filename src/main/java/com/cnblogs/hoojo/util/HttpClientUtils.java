2d54d1db4a5fc3e91aee53592dd3315b81799ca9
hoojo <hoojo@qq.com>
2019-04-18 16:27:16 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b> http client request

---
+++ src/main/java/com/cnblogs/hoojo/util/HttpClientUtils.java
@@ -0,0 +1,169 @@
+package com.cnblogs.hoojo.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * <b>function:</b> http client request
+ * 
+ * @author hoojo
+ * @createDate 2019年4月18日 下午3:19:02
+ * @file HttpClientUtils.java
+ * @package com.cnblogs.hoojo.util
+ * @project PhotoSpider
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class HttpClientUtils {
+
+	public static String get(String requestURL, String charset) throws Exception {
+		HttpURLConnection connection = null;
+		InputStream is = null;
+		BufferedReader br = null;
+		String result = null;// 返回结果字符串
+		try {
+			// 创建远程url连接对象
+			URL url = new URL(requestURL);
+			// 通过远程url连接对象打开一个连接，强转成httpURLConnection类
+			connection = (HttpURLConnection) url.openConnection();
+			// 设置连接方式：get
+			connection.setRequestMethod("GET");
+			// 设置连接主机服务器的超时时间：15000毫秒
+			connection.setConnectTimeout(15000);
+			// 设置读取远程返回的数据时间：60000毫秒
+			connection.setReadTimeout(60000);
+			// 发送请求
+			connection.connect();
+			
+			// 通过connection连接，获取输入流
+			if (connection.getResponseCode() == 200) {
+				is = connection.getInputStream();
+				// 封装输入流is，并指定字符集
+				br = new BufferedReader(new InputStreamReader(is, charset));
+				
+				// 存放数据
+				StringBuffer buff = new StringBuffer();
+
+				char[] cbuf = new char[1024];
+	            int length;
+	            while ((length = br.read(cbuf)) != -1) {
+	                buff.append(cbuf, 0, length);
+	            }
+	            
+				result = buff.toString();
+			}
+		} catch (MalformedURLException e) {
+			throw e;
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			// 关闭资源
+			if (null != br) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (null != is) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			connection.disconnect();// 关闭远程连接
+		}
+
+		return result;
+	}
+
+	public static String post(String requestURL, String param, String charset) throws Exception {
+
+		HttpURLConnection connection = null;
+		InputStream is = null;
+		OutputStream os = null;
+		BufferedReader br = null;
+		String result = null;
+		try {
+			URL url = new URL(requestURL);
+			// 通过远程url连接对象打开连接
+			connection = (HttpURLConnection) url.openConnection();
+			// 设置连接请求方式
+			connection.setRequestMethod("POST");
+			// 设置连接主机服务器超时时间：15000毫秒
+			connection.setConnectTimeout(15000);
+			// 设置读取主机服务器返回数据超时时间：60000毫秒
+			connection.setReadTimeout(60000);
+
+			// 默认值为：false，当向远程服务器传送数据/写数据时，需要设置为true
+			connection.setDoOutput(true);
+			// 默认值为：true，当前向远程服务读取数据时，设置为true，该参数可有可无
+			connection.setDoInput(true);
+			// 设置传入参数的格式:请求参数应该是 name1=value1&name2=value2 的形式。
+			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36");
+			// 通过连接对象获取一个输出流
+			os = connection.getOutputStream();
+			// 通过输出流对象将参数写出去/传输出去,它是通过字节数组写出的
+			os.write(param.getBytes());
+			
+			// 通过连接对象获取一个输入流，向远程读取
+			if (connection.getResponseCode() == 200) {
+
+				is = connection.getInputStream();
+				// 对输入流对象进行包装:charset根据工作项目组的要求来设置
+				br = new BufferedReader(new InputStreamReader(is, charset));
+
+				StringBuffer buff = new StringBuffer();
+
+				char[] cbuf = new char[1024];
+	            int length;
+	            while ((length = br.read(cbuf)) != -1) {
+	                buff.append(cbuf, 0, length);
+	            }
+	            
+				result = buff.toString();
+			}
+		} catch (MalformedURLException e) {
+			throw e;
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			// 关闭资源
+			if (null != br) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (null != os) {
+				try {
+					os.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (null != is) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			// 断开与远程地址url的连接
+			connection.disconnect();
+		}
+		return result;
+	}
+}
